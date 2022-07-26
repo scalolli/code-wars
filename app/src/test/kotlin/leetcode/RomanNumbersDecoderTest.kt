a8e4af4369446148d35084f92687fa64bdc624b2
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2022-07-26 20:57:33 +0100

    Uncomment tests

--- app/src/test/kotlin/leetcode/RomanNumbersDecoderTest.kt
+++ app/src/test/kotlin/leetcode/RomanNumbersDecoderTest.kt
@@ -7,12 +7,12 @@ class RomanNumbersDecoderTest {
 
     @Test
     fun decodeItoX() {
-//        Assert.assertEquals(1, RomanNumbersDecoder.romanToInt("I"))
-//        Assert.assertEquals(3, RomanNumbersDecoder.romanToInt("III"))
-//        Assert.assertEquals(4, RomanNumbersDecoder.romanToInt("IV"))
-//        Assert.assertEquals(5, RomanNumbersDecoder.romanToInt("V"))
-//        Assert.assertEquals(6, RomanNumbersDecoder.romanToInt("VI"))
-//        Assert.assertEquals(9, RomanNumbersDecoder.romanToInt("IX"))
+        Assert.assertEquals(1, RomanNumbersDecoder.romanToInt("I"))
+        Assert.assertEquals(3, RomanNumbersDecoder.romanToInt("III"))
+        Assert.assertEquals(4, RomanNumbersDecoder.romanToInt("IV"))
+        Assert.assertEquals(5, RomanNumbersDecoder.romanToInt("V"))
+        Assert.assertEquals(6, RomanNumbersDecoder.romanToInt("VI"))
+        Assert.assertEquals(9, RomanNumbersDecoder.romanToInt("IX"))
         Assert.assertEquals(10, RomanNumbersDecoder.romanToInt("X"))
     }
 
