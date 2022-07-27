084c50f98cc4252c9ac7c5a43ed016c458dbe8fd
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2022-07-27 18:45:22 +0100

    solution to merging two sorted lists

--- app/src/test/kotlin/code/wars/MergeSortedListSolution.kt
+++ app/src/test/kotlin/code/wars/MergeSortedListSolution.kt
@@ -2,8 +2,57 @@ package code.wars
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+
+    constructor(value: Int, next: ListNode?) : this(value) {
+        this.next = next
+    }
 }
 
 fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
-    TODO()
+    if (list1 == null)
+        return list2
+
+    if (list2 == null)
+        return list1
+
+    var currentNodeFromList1: ListNode? = list1
+    var currentNodeFromList2: ListNode? = list2
+    var currentNode: ListNode? = null
+    var firstNodeOfResult: ListNode? = null
+    var newNode: ListNode?
+
+    while (currentNodeFromList1 != null && currentNodeFromList2 != null) {
+        if (currentNodeFromList1.`val` <= currentNodeFromList2.`val`) {
+            newNode = ListNode(currentNodeFromList1.`val`)
+            currentNodeFromList1 = currentNodeFromList1?.next
+        } else {
+            newNode = ListNode(currentNodeFromList2.`val`)
+            currentNodeFromList2 = currentNodeFromList2?.next
+        }
+
+        if (firstNodeOfResult == null)
+            firstNodeOfResult = newNode
+
+        currentNode?.next = newNode
+        currentNode = newNode
+    }
+
+    if (currentNodeFromList1 == null)
+        mergeLists(currentNode, currentNodeFromList2)
+    else if (currentNodeFromList2 == null)
+        mergeLists(currentNode, currentNodeFromList1)
+
+    return firstNodeOfResult
+}
+
+fun mergeLists(list1: ListNode?, list2: ListNode?) {
+    var currentNode = list1
+    var currentNodeForSecondList = list2
+
+    while (currentNodeForSecondList != null) {
+        val newNode = ListNode(currentNodeForSecondList.`val`)
+        currentNode?.next = newNode
+        currentNode = newNode
+        currentNodeForSecondList = currentNodeForSecondList.next
+    }
 }
--- app/src/test/kotlin/leetcode/MergeSortedListsTests.kt
+++ app/src/test/kotlin/leetcode/MergeSortedListsTests.kt
@@ -9,9 +9,32 @@ class MergeSortedListsTests {
 
     @Test
     fun basicScenario() {
-        val node1 = ListNode(1)
-        val node2 = ListNode(0)
-        node2.next = node1
-        Assert.assertEquals(node2, mergeTwoLists(ListNode(1), ListNode(0)))
+        val list1 = ListNode(1, null)
+        val list2 = ListNode(0, null)
+        Assert.assertEquals(listOf(0, 1), convertLinkedListToArray(mergeTwoLists(list1, list2)))
+    }
+
+    @Test
+    fun complexScenarios() {
+        val list1 = ListNode(1, ListNode(2, ListNode(4, null)))
+        val list2 = ListNode(1, ListNode(3, ListNode(4, null)))
+        Assert.assertEquals(listOf(1, 1, 2, 3, 4, 4), convertLinkedListToArray(mergeTwoLists(list1, list2)))
+    }
+
+    @Test
+    fun cornerCases() {
+        Assert.assertEquals(listOf(0), convertLinkedListToArray(mergeTwoLists(null, ListNode(0, null))))
+    }
+
+    private fun convertLinkedListToArray(listNode: ListNode?): MutableList<Int> {
+        var currentNode = listNode
+        var values = mutableListOf<Int>()
+
+        while (currentNode != null) {
+            values += currentNode.`val`
+            currentNode = currentNode.next
+        }
+
+        return values
     }
 }
