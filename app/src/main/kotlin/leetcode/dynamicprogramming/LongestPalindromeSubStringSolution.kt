f340eb96d39aaa4a7be08065f3cf32f55c2b850f
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2022-08-09 07:18:28 +0100

    implement solution which looks for palindrome from middle

--- app/src/main/kotlin/leetcode/dynamicprogramming/LongestPalindromeSubStringSolution.kt
+++ app/src/main/kotlin/leetcode/dynamicprogramming/LongestPalindromeSubStringSolution.kt
@@ -7,41 +7,20 @@ object LongestPalindromeSubStringSolution {
     fun longestPalindrome(s: String): String {
         if (s.isEmpty()) return ""
 
-//        var start = 0
-//        var end = 0
-//
-//        for (i in s.indices) {
-//            val len1 = checkForPalindromeFromMiddle(i, i, s)
-//            val len2 = checkForPalindromeFromMiddle(i, i + 1, s)
-//            val maxLength = max(len1, len2)
-//            if (maxLength > (end - start)) {
-//                start = i - ((maxLength - 1) / 2)
-//                end = i + (maxLength / 2)
-//            }
-//        }
-//
-//        return s.slice(start..end)
+        var start = 0
+        var end = 0
 
-        val palindromes = mutableListOf<String>()
-
-        s.forEach { palindromes.add(it.toString()) }
-
-        for (index in s.indices) {
-            checkForPalindrome(palindromes, s[index], s.substring(index + 1 until s.length))
+        for (i in s.indices) {
+            val len1 = checkForPalindromeFromMiddle(i, i, s)
+            val len2 = checkForPalindromeFromMiddle(i, i + 1, s)
+            val maxLength = max(len1, len2)
+            if (maxLength > (end - start)) {
+                start = i - ((maxLength - 1) / 2)
+                end = i + (maxLength / 2)
+            }
         }
 
-        palindromes.sortWith(compareByDescending<String> { it.length }.thenBy { it })
-        return palindromes.first()
-    }
-
-    private fun checkForPalindrome(palindromes: MutableList<String>, currentChar: Char, remainingString: String) {
-        for (index in remainingString.indices) {
-            val currentWord = currentChar.toString() + remainingString.subSequence(0..index)
-            if (palindromes.contains(currentWord))
-                continue
-            else if (currentWord == currentWord.reversed())
-                palindromes.add(currentWord)
-        }
+        return s.slice(start..end)
     }
 
     private fun checkForPalindromeFromMiddle(left: Int, right: Int, input: String): Int {
