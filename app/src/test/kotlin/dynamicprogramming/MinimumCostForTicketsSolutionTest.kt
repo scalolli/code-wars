3172fccdece279c9637575e889977c2164a03f50
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2022-08-22 12:26:15 +0100

    uncomment some tests

--- app/src/test/kotlin/dynamicprogramming/MinimumCostForTicketsSolutionTest.kt
+++ app/src/test/kotlin/dynamicprogramming/MinimumCostForTicketsSolutionTest.kt
@@ -9,6 +9,6 @@ class MinimumCostForTicketsSolutionTest {
     @Test
     fun testSimple() {
         assertEquals(6, mincostTickets(intArrayOf(1, 2, 3), intArrayOf(2, 7, 15)))
-//        assertEquals(5, mincostTickets(intArrayOf(1, 2, 3), intArrayOf(2, 5, 15)))
+        assertEquals(5, mincostTickets(intArrayOf(1, 2, 3), intArrayOf(2, 5, 15)))
     }
 }
